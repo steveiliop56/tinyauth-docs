ef2d6e16feb266b6ea5f23238d075f14f020848e
Stavros <steveiliop56@gmail.com>
2025-10-03 21:15:03 +0300

    fix: disable ssr on react router

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,7 +5,7 @@ import { createGetUrl, getSlugs } from "fumadocs-core/source";
 const getUrl = createGetUrl("/docs");
 
 export default {
-  ssr: true,
+  ssr: false,
   async prerender({ getStaticPaths }) {
     const paths: string[] = [...getStaticPaths()];
     for await (const entry of glob("**/*.mdx", { cwd: "content/docs" })) {
