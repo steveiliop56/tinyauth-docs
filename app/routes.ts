cdc140f675e560823673185a6b75808ce459849f
Stavros <steveiliop56@gmail.com>
2025-10-07 17:55:32 +0300

    feat: add route to redirect to discord

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,8 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes';
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index('routes/home.tsx'),
-  route('docs/*', 'docs/page.tsx'),
-  route('api/search', 'docs/search.ts'),
+  index("routes/home.tsx"),
+  route("discord", "routes/discord.ts"),
+  route("docs/*", "docs/page.tsx"),
+  route("api/search", "docs/search.ts"),
 ] satisfies RouteConfig;
